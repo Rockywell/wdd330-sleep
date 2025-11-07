b9c7bb78f27534e0548f92f1b71cf47daed85d8e
kelwood22 <kwoodland@byupathway.edu>
2025-11-07 15:06:42 -0700

    added a superscript to the card icon

--- src/js/cart.js
+++ src/js/cart.js
@@ -26,3 +26,11 @@ function cartItemTemplate(item) {
 }
 
 renderCartContents();
+
+function updateCartCount() {
+  const cartCountElement = document.querySelector('.cart-count');
+  const cart = JSON.parse(localStorage.getItem('so-cart')) || [];
+  cartCountElement.textContent = cart.length;
+}
+
+document.addEventListener('DOMContentLoaded', updateCartCount);
--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,12 @@ const listElement = document.querySelector('.product-list');
 
 const tentList = new ProductList('tents', dataSource, listElement);
 tentList.init();
+
+function updateCartCount() {
+    const cartCountElement = document.querySelector('.cart-count');
+    const cart = JSON.parse(localStorage.getItem('so-cart')) || [];
+    cartCountElement.textContent = cart.length;
+}
+
+document.addEventListener('DOMContentLoaded', updateCartCount);
+
