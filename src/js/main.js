8383b2727b47e675a8d3e1b020910a113dcb0008
Rockywell <47404779+Rockywell@users.noreply.github.com>
2025-11-10 23:48:04 -0700

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 function updateCartCount() {
-    const cartCountElement = document.querySelector('.cart-count');
-    const cart = JSON.parse(localStorage.getItem('so-cart')) || [];
-    cartCountElement.textContent = cart.length;
+  const cartCountElement = document.querySelector('.cart-count');
+  const cart = getLocalStorage("so-cart") ?? [];
+  cartCountElement.textContent = cart.length;
 }
 
 document.addEventListener('DOMContentLoaded', updateCartCount);
