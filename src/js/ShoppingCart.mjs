abfdc25058e9cddaec5fb9ddc0891e573014f080
kelwood22 <kwoodland@byupathway.edu>
2025-11-14 15:03:07 -0700

    Added ability to change quantities in cart

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -10,7 +10,10 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors?.[0]?.ColorName ?? ""}</p>
-  <p class="cart-card__quantity">qty: 1</p>
+  <label class="cart-card__quantity">
+    Qty:
+    <input type="number" min="1" value="${item.quantity ?? 1}" class="quantity-input" data-id="${item.Id}" />
+    </label>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
 }
@@ -35,13 +38,29 @@ export default class ShoppingCart {
         if (this.cartItems.length === 0) {
             this.productList.innerHTML = `<p class="empty-cart">Your cart is currently empty.</p>`;
         } else {
-            let total = this.cartItems.reduce((sum, item) => sum + item.FinalPrice, 0);
+            let total = this.cartItems.reduce((sum, item) => sum + (item.FinalPrice ?? item.ListPrice) * (item.quantity ?? 1), 0);
 
             const cartFooter = document.querySelector(".cart-footer");
             const cartTotal = document.querySelector(".cart-total");
 
 
             renderListWithTemplate(cartItemTemplate, this.productList, this.cartItems);
+            this.productList.querySelectorAll('.quantity-input').forEach(input => {
+                input.addEventListener('change', (event) => {
+                    const newQuantity = parseInt(event.target.value);
+                    const productId = event.target.dataset.id;
+
+                    const item = this.cartItems.find(p => p.Id === productId);
+                    if (item && newQuantity > 0) {
+                        item.quantity = newQuantity;
+                        item.FinalPrice = item.ListPrice * newQuantity;
+                        localStorage.setItem("so-cart", JSON.stringify(this.cartItems));
+                        this.renderCartContents();
+
+                    }
+                });
+            });
+
             cartTotal.textContent = `($${total})`;
             cartFooter.classList.toggle("hide");
         }
