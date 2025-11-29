d91cd268b86f02499e9e0e47d543647473e3e6fe
Celine Mafigu <cmafigu@byupathwa.edu>
2025-11-30 00:03:36 +0200

    initial commit

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,9 +1,97 @@
-import ShoppingCart from "./ShoppingCart.mjs";
-
-//HTML Functions
-
 // wrapper for querySelector...returns matching element
-export const qs = (selector, parent = document) => parent.querySelector(selector);
+export function qs(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+// or a more concise version if you are into that sort of thing:
+// export const qs = (selector, parent = document) => parent.querySelector(selector);
+
+
+/**
+ * Grabs a value from localStorage by the specified key.
+ * Returns an empty array if the key does not exist.
+ *
+ * @param {string} key - The key to retrieve from localStorage.
+ * @returns {*} The parsed value from localStorage, or an empty array if not found.
+ */
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key)) || [];
+}
+
+
+/**
+ * Stores data in the browser's localStorage under the specified key, overwriting any existing value.
+ * If you want to add to an existing array in localStorage instead of overwriting it, use addToLocalStorage().
+ *
+ * @param {string} key - The key under which the data will be stored.
+ * @param {*} data - The data to be stored.
+ */
+export function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+
+/**
+ * Adds a new item to the beginning of an array stored in localStorage under the specified key.
+ * If there is no array associated with the key, creates a new array with the data item.
+ *
+ * @param {string} key - The key under which the array is stored in localStorage.
+ * @param {*} data - The data item to add to the array.
+ */
+export function addToLocalStorage(key, data) {
+  const localData = getLocalStorage(key);
+  localData.unshift(data);
+  setLocalStorage(key, localData);
+}
+
+
+/**
+ * Grabs the value of a specified query parameter from the current page's URL.
+ *
+ * @param {string} param - The name of the query parameter to retrieve.
+ * @returns {string} The value of the specified query parameter.
+ */
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const value = urlParams.get(param);
+  return value;
+}
+
+export function renderListWithTemplate(list, parentElement, templateFn, position = "beforeend", clear = false) {
+  const html = list.map(templateFn).join("");
+
+  if (clear) parentElement.innerHTML = "";
+
+  parentElement.insertAdjacentHTML(position, html);
+}
+
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+
+  parentElement.innerHTML = template;
+
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) {
+  const response = await fetch(path);
+  const template = await response.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("/partials/header.html");
+  const footerTemplate = await loadTemplate("/partials/footer.html");
+
+  const headerElement = document.querySelector("header");
+  const footerElement = document.querySelector("footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+
+}
 
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
@@ -14,65 +102,46 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
-export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
-  if (clear) parentElement.replaceChildren();
-
-  let listTemplate = list.map(templateFn).join('');
-  parentElement.insertAdjacentHTML(position, listTemplate);
+export function capitalize(string) {
+  return string[0].toUpperCase() + string.slice(1);
 }
 
-export function renderWithTemplate(template, parentElement, data, callback) {
+export function formatNumToCurrency(num) {
+  const formattedNum = new Intl.NumberFormat(undefined, {
+    style: "currency",
+    currency: "USD"
+  }).format(num);
 
-  parentElement.innerHTML = template;
 
-  if (callback) callback(data);
-}
-
-export async function loadTemplate(path) {
-  const res = await fetch(path);
-  const template = await res.text();
-
-  return template;
-}
-
-export async function loadHeaderFooter() {
-  const headerTemplate = await loadTemplate("../partials/header.html");
-  const headerElement = document.querySelector("#main-header");
-
-  const footerTemplate = await loadTemplate("../partials/footer.html");
-  const footerElement = document.querySelector("#main-footer");
-
-  renderWithTemplate(headerTemplate, headerElement);
-  renderWithTemplate(footerTemplate, footerElement);
-
-  ShoppingCart.updateCartCount();
-}
-
-//Window Functions
-
-export function getParam(param) {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-
-  return urlParams.get(param);
+  return formattedNum;
 }
 
 
-//Local Storage
+export function alertMessage(message, scroll = true, durration = 0) {
+  const alert = document.createElement("div");
+  alert.classList.add("alert");
 
-// retrieve data from localstorage
-export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
-}
-// save data to local storage
-export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
-// adds one or multiple data items to a local storage array
-export function addLocalStorage(key, ...data) {
-  let storage = getLocalStorage(key) ?? [];
+  const alertMessage = document.createElement("p");
+  const removeButton = document.createElement("span");
 
-  const combinedData = [].concat(storage, data);
+  alertMessage.textContent = message;
+  removeButton.textContent = "X";
 
-  setLocalStorage(key, combinedData);
+  removeButton.addEventListener("click", event => {
+    event.target.parentElement.remove();
+  })
+  alert.append(alertMessage, removeButton);
+
+
+  document.querySelector("main").insertAdjacentElement("afterbegin", alert);
+
+  if (scroll) {
+    window.scrollTo(0, 0);
+  }
+
+  if (durration) {
+    setTimeout(() => {
+      alert.remove();
+    }, durration);
+  }
 }
