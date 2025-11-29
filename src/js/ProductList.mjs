cb066b71f9f8405adf52f8cfd507e1150c8b2895
mjgifford3485 <mjgifford3485@gmail.com>
2025-11-29 13:28:39 -0800

    added sort option

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -16,17 +16,58 @@ export default class ProductList {
         this.category = category;
         this.dataSource = dataSource;
         this.listElement = listElement;
+        this.products = [];
     };
 
     async init() {
         document.querySelector(".title").textContent = this.category.charAt(0).toUpperCase() + this.category.slice(1);
-
-        let products = await this.dataSource.getData(this.category);
-        this.renderList(products);
+        this.products = await this.dataSource.getData(this.category);
+        this.initSort();
+        this.renderList(this.products);
     }
 
     renderList(list) {
         renderListWithTemplate(productCardTemplate, this.listElement, list);
+    }
+
+    initSort() {
+        const sortSelect = document.querySelector("#sort");
+        if (!sortSelect) return;
+
+        sortSelect.addEventListener("change", () => {
+            const sortBy = sortSelect.value;
+            const sorted = this.sortProducts(this.products, sortBy);
+            this.renderList(sorted);
+        });
+    }
+
+    sortProducts(list, sortBy) {
+        const sorted = [...list];
+        switch (sortBy) {
+            case "name-asc":
+                sorted.sort((a, b) =>
+                    a.NameWithoutBrand.localeCompare(b.NameWithoutBrand)
+                );
+                break;
+            case "name-desc":
+                sorted.sort((a, b) =>
+                    b.NameWithoutBrand.localeCompare(a.NameWithoutBrand)
+                );
+                break;
+            case "price-asc":
+                sorted.sort((a, b) =>
+                    a.FinalPrice - b.FinalPrice
+                );
+                break;
+            case "price-desc":
+                sorted.sort((a, b) =>
+                    b.FinalPrice - a.FinalPrice
+                );
+                break;
+            default:
+        }
+        
+        return sorted;
     }
 }
 
